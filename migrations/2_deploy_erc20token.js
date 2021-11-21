8c185e3423cd9e3f85d1830fd0bca062ffa8e646
defifofum <defifofum0@gmail.com>
2021-11-21 17:53:53 -0500

    feat: Add ERC20 contract and deployment

---
+++ migrations/2_deploy_erc20token.js
@@ -0,0 +1,8 @@
+const ERC20Token = artifacts.require("ERC20Token");
+
+// constructor(string name, string symbol) ERC20("ApeSwapFinance - Ola Farm", "OLA-FARM") {
+module.exports = async function (deployer) {
+  const tokenName = "";
+  const tokenSymbol = "";
+  await deployer.deploy(ERC20Token, tokenName, tokenSymbol);
+};
