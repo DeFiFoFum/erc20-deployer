5418755790678bee02ce005adbda56d45521e883
defifofum <defifofum0@gmail.com>
2022-11-01 22:24:51 -0300

    refactor: Formatting

--- contracts/ERC20TokenFactory.sol
+++ contracts/ERC20TokenFactory.sol
@@ -4,28 +4,36 @@ pragma solidity 0.8.3;
 import "./ERC20Token.sol";
 
 contract ERC20TokenFactory {
-
-    event CreateERC20Token(address indexed tokenAddress, string name, string indexed symbol, uint256 decimals);
+    event CreateERC20Token(
+        address indexed tokenAddress,
+        string name,
+        string indexed symbol,
+        uint256 decimals
+    );
 
     address[] public tokenAddresses;
 
     function createERC20Token(
-        string memory name, 
-        string memory symbol, 
+        string memory name,
+        string memory symbol,
         uint256 amount
     ) external {
-        address erc20Token = address(new ERC20Token(name, symbol, amount, 18, msg.sender));
+        address erc20Token = address(
+            new ERC20Token(name, symbol, amount, 18, msg.sender)
+        );
         tokenAddresses.push(erc20Token);
         emit CreateERC20Token(erc20Token, name, symbol, 18);
     }
 
     function createERC20TokenWithDecimals(
-        string memory name, 
-        string memory symbol, 
-        uint256 amount, 
+        string memory name,
+        string memory symbol,
+        uint256 amount,
         uint8 tokenDecimals
     ) external {
-        address erc20Token = address(new ERC20Token(name, symbol, amount, tokenDecimals, msg.sender));
+        address erc20Token = address(
+            new ERC20Token(name, symbol, amount, tokenDecimals, msg.sender)
+        );
         tokenAddresses.push(erc20Token);
         emit CreateERC20Token(erc20Token, name, symbol, tokenDecimals);
     }
